4baa5324d441b80110d00350c67cd8cfb6de7621
suongfiori <suong.kf@gmail.com>
2023-02-07 19:15:45 +0000

    Merging components

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,13 +1,25 @@
 import React from "react"
 
-export default function Time (props) {
-    const minutes = ("0" + Math.floor(props.timer / 60)).slice(-2)
-    const seconds = ("0" + props.timer % 60).slice(-2)
+export default function Time ({ time, type, timer, bestScore }) {
+
+ // Helper function to format time 
+    function formatTime (time){
+        return time < 10 ? "0" + time : time;
+    } 
+
+    let timeString;
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    timeString = `${formatTime(minutes)}:${formatTime(seconds)}`;
+
+    const styles = {
+        color: type === "bestScore" ? "rgb(64, 160, 64)" : "none"
+    }
 
     return (
         <div >
-            <h4>Current</h4>
-            <span className="timer">{minutes} : {seconds}</span>  
+        <h4 className={`${type==="bestScore" ? "best-score" : ""}`}>{type === "timer" ? "Current" : "Your Best"}</h4>
+            <span className="timer" style={styles}>{timeString}</span>  
         </div>
     )
 }
